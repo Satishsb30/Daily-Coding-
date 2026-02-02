a1a1b67a1583e3c7764fb4303bb4dbe50bdf9119
Satish Siddappa budanur <budunursatishsb@gmail.com>
2026-02-02 23:36:04 +0530

    Add files via upload

---
+++ day-1.py
@@ -0,0 +1,15 @@
+"""1️⃣ Add two numbers
+2️⃣ Find square of a number
+3️⃣ Check even or odd
+4️⃣ Swap two variables
+5️⃣ Take name as input and print greeting"""
+a = int(input("enter a value of a : "))
+b= int(input("enter the vakue of b : "))
+c = a+ b
+e = a*a
+print(e)
+if a % 2==0:
+    print("entered number is even" )
+
+else :
+    print("entered value is odd")
